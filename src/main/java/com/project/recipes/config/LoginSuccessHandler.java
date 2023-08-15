8ae621b6b600a40ce6454112252b29f8ae405f53
Ivan Vynnychuk <ivavyn@gmail.com>
2023-08-15 22:21:30 +0300

    fix: fix bug

--- src/main/java/com/project/recipes/config/LoginSuccessHandler.java
+++ src/main/java/com/project/recipes/config/LoginSuccessHandler.java
@@ -8,22 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
-public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) throws IOException {
-        PersonSecurity userDetails = (PersonSecurity) authentication.getPrincipal();
-
-        String redirectURL = request.getContextPath();
-
-        if (userDetails.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ADMIN"))) {
-            redirectURL += "/users/";
-        } else {
-            redirectURL += "/recipes/all/users/" + userDetails.getId();
-        }
-        getRedirectStrategy().sendRedirect(request, response, redirectURL);
-    }
-}
 
 
 @Component
